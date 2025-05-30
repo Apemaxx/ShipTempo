43d2834539001109b5f5881ccd2f6577ac2ff174
A2RG <ceo@a2rg.com>
2025-05-29 19:58:33 -0500

    Corrects the display of null descriptions in milestones, showing a dash instead of "null" in CFSCargoDetailsPage.

--- src/components/cfs/CFSCargoDetails.tsx
+++ src/components/cfs/CFSCargoDetails.tsx
@@ -404,9 +404,9 @@ const CFSCargoDetailsPage: React.FC = () => {
                         {milestone.code}
                       </td>
                       <td className="p-3 border-r border-gray-300">
-                        {milestone.description === null
-                          ? "-"
-                          : milestone.description}
+                        {milestone.description !== "null"
+                          ? milestone.description
+                          : "-"}
                       </td>
                       <td className="p-3 border-r border-gray-300">
                         {milestone.statusDateTime}
